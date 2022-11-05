56f0154ecdb6a83dc4bcda57fc420c45fdd44d32
Mia Tanner <mia@indygecko.com>
2022-11-05 01:42:18 -0700

    refactor getAllProperties to accept filtering

--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -115,16 +115,69 @@ exports.getAllReservations = getAllReservations;
  * @param {*} limit The number of results to return.
  * @return {Promise<[{}]>}  A promise to the properties.
  */
-const getAllProperties = (options, limit = 10) => {
+const getAllProperties = function(options, limit = 10) {
+  const queryParams = [];
+  let queryString = `
+  SELECT properties.*, avg(property_reviews.rating) as average_rating
+  FROM properties
+  JOIN property_reviews ON properties.id = property_id
+  `;
 
-  return pool
-    .query(`
-    SELECT * FROM properties
-    LIMIT $1
-    `, [limit])
-    .then((result) => {
-      return (result.rows);
-    })
+  if (options.city) {
+    queryParams.push(`%${options.city}%`);
+    queryString += `WHERE city LIKE $${queryParams.length} `;
+  }
+
+  if (options.owner_id) {
+    if (queryParams.length === 0) {
+      queryString += 'WHERE ';
+    } else {
+      queryString += 'AND ';
+    }
+    queryParams.push(`${options.owner_id}`);
+    
+    queryString += `owner_id = $${queryParams.length} `;
+  }
+
+  if (options.minimum_price_per_night) {
+    if (queryParams.length === 0) {
+      queryString += 'WHERE ';
+    } else {
+      queryString += 'AND ';
+    }
+    queryParams.push(options.minimum_price_per_night * 100);
+    queryString += `cost_per_night >= $${queryParams.length}`;
+  }
+
+  if (options.maximum_price_per_night) {
+    if (queryParams.length === 0) {
+      queryString += 'WHERE ';
+    } else {
+      queryString += 'AND ';
+    }
+    queryParams.push(options.maximum_price_per_night * 100);
+    queryString += `cost_per_night <= $${queryParams.length}`;
+  }
+
+ 
+  queryString += `
+  GROUP BY properties.id
+  `;
+
+  if (options.minimum_rating) {
+    queryParams.push(options.minimum_rating);
+    queryString += `HAVING avg(property_reviews.rating) >= $${queryParams.length}`;
+  }
+
+  queryParams.push(limit);
+  queryString += `
+  ORDER BY cost_per_night
+  LIMIT $${queryParams.length};
+  `;
+
+  //console.log(queryString, queryParams);
+
+  return pool.query(queryString, queryParams).then((res) => res.rows)
     .catch((err) => {
       console.log(err.message);
     });
